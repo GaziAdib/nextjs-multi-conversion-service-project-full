1bd3c635b0284678426e023aa50f965dba091e56
unknown <adibcse10@gmail.com>
2023-03-09 01:42:10 +0600

    added user defined quality and max wisth and max height in image reducer service

--- src/pages/imageSizeReducerService/index.js
+++ src/pages/imageSizeReducerService/index.js
@@ -4,20 +4,31 @@ import Image from 'next/image';
 
 import Resizer from 'react-image-file-resizer';
 
+import InputRange from 'react-input-range';
+
+import 'react-input-range/lib/css/index.css';
+
 
 const ImageSizeReducer = () => {
 
+
+
     const [imageFile, setImageFile] = useState("");
     const [outputImage, setOutputImage] = useState("");
+    const [value, setValue] = useState({ min: 0, max: 80 });
+
+    const [maxWidth, setMaxWidth] = useState(300);
+    const [maxHeight, setMaxHeight] = useState(200);
+
 
     const resizeFile = (file) =>
         new Promise((resolve) => {
             Resizer.imageFileResizer(
                 file,
-                400,
-                500,
+                maxWidth,
+                maxHeight,
                 "JPEG",
-                90,
+                value?.max,
                 0,
                 (uri) => {
                     resolve(uri);
@@ -52,19 +63,34 @@ const ImageSizeReducer = () => {
         <>
             <h1 className='text-center mx-auto mt-5 mb-2 py-3 text-2xl font-bold text-green-600'>Image Size Reducer</h1>
 
+            <InputRange
+                minValue={0}
+                maxValue={100}
+                value={value}
+                onChange={setValue}
+            />
+
+            <input type="number" placeholder='MaxWidth' name='max width' onChange={(e) => setMaxWidth(e.target.value)} />
+
+            <input type="number" placeholder='MaxHeight' name='max height' onChange={(e) => setMaxHeight(e.target.value)} />
+
+
             <input type="file" className="" onChange={(e) => setImageFile(e.target.files[0])} />
 
             <button onClick={() => handleImageFileSizeReducer(imageFile)} className='bg-red-400 rounded-sm shadow-md mt-5 mb-2 py-1 px-1 text-center' >Reduce Size</button>
 
-            <a href={outputImage} download>
-                <Image
-                    src={outputImage}
-                    priority
-                    height={300}
-                    width={200}
-                    alt="output image"
-                />
-            </a>
+            <div>
+
+                <a className='w-[300] h-[200]' href={outputImage} download>
+                    <Image
+                        src={outputImage}
+                        priority
+                        height={300}
+                        width={200}
+                        alt="output image"
+                    />
+                </a>
+            </div>
 
             {/* <img src={outputImage?.name} className='mx-auto mt-5 mb-2 py-2' /> */}
         </>
