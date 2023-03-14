afc2b91daff7bf304c67811e12d53d737122ef1f
unknown <adibcse10@gmail.com>
2023-03-14 23:54:53 +0600

    added image Editor feature uidesign

---
+++ src/pages/imageEditorService/index.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+import { GrRotateLeft, GrRotateRight } from 'react-icons/gr';
+
+import { CgMergeVertical, CgMergeHorizontal } from 'react-icons/cg';
+
+import { IoMdUndo, IoMdRedo, IoIosImage } from 'react-icons/io';
+
+const ImageEditor = () => {
+
+    const filterElement = [
+        {
+            name: 'brightness',
+        },
+        {
+            name: 'grayscale',
+        },
+        {
+            name: 'sepia',
+        },
+        {
+            name: 'saturate',
+        },
+        {
+            name: 'contrast',
+        },
+        {
+            name: 'hueRotate',
+        }
+    ]
+
+    const [selectedImage, setSelectedImage] = useState('');
+
+
+    return (
+        <>
+            <div className='image_editor'>
+                <div className='card'>
+                    <div className='card_header'>
+                        <h2>---- Image Editor ----</h2>
+                    </div>
+                    <div className='card_body'>
+                        <div className='sidebar'>
+                            <div className='side_body'>
+                                <div className='filter_section'>
+                                    <span>Filters</span>
+                                    <div className='filter_key'>
+                                        {
+                                            filterElement?.map((v, i) => <button key={i}>{v.name}</button>)
+                                        }
+                                    </div>
+                                </div>
+
+                                <div className='filter_slider'>
+                                    <div className='label_bar'>
+                                        <label>Rotate</label>
+                                        <span>100%</span>
+                                    </div>
+                                    <input type='range' min='0' max='200' />
+                                </div>
+
+
+                                <div className='rotate'>
+                                    <label>Rotate & Flip</label>
+                                    <div className='icon'>
+                                        <div><GrRotateLeft /></div>
+                                        <div><GrRotateRight /></div>
+                                        <div><CgMergeVertical /></div>
+                                        <div><CgMergeHorizontal /></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='reset'>
+                                <button>Reset</button>
+                                <button className='save'>Save</button>
+                            </div>
+
+
+                        </div>
+
+
+                        <div className='image_section'>
+                            <div className='image'>
+                                <label htmlFor='choose'>
+                                    <IoIosImage />
+                                    <span>Choose Image</span>
+                                </label>
+                            </div>
+
+                            <div className='image_select'>
+                                <button className='undo'><IoMdUndo /></button>
+                                <button className='redo'><IoMdRedo /></button>
+                                <button className='crop'>Crop Image</button>
+                                <label htmlFor='choose'>Choose Image</label>
+                                <input type='file' id='choose' />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ImageEditor
