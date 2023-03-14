12cbaf181e4f9c4e87474b7570fbee03f0149bb8
unknown <adibcse10@gmail.com>
2023-03-15 03:27:30 +0600

    added awesome photo edit features not yet completed

--- src/pages/imageEditorService/index.js
+++ src/pages/imageEditorService/index.js
@@ -8,28 +8,95 @@ import { IoMdUndo, IoMdRedo, IoIosImage } from 'react-icons/io';
 
 const ImageEditor = () => {
 
+    const [state, setState] = useState({
+        image: '',
+        brightness: 100,
+        grayscale: 0,
+        sepia: 0,
+        hueRotate: 0,
+        saturate: 100,
+        contrast: 100,
+        rotate: 0,
+        vertical: 1,
+        horizontal: 1
+    })
+
     const filterElement = [
         {
             name: 'brightness',
+            maxValue: 200
         },
         {
             name: 'grayscale',
+            maxValue: 200
         },
         {
             name: 'sepia',
+            maxValue: 200
         },
         {
             name: 'saturate',
+            maxValue: 200
         },
         {
             name: 'contrast',
+            maxValue: 200
         },
         {
-            name: 'hueRotate',
+            name: 'hueRotate'
         }
     ]
 
-    const [selectedImage, setSelectedImage] = useState('');
+    const [property, setProperty] = useState(
+        {
+            name: 'brightness',
+            maxValue: 200
+        },
+    )
+
+
+    const imageHandle = (e) => {
+        if (e.target.files?.length !== 0) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setState({
+                    ...state,
+                    image: reader.result
+                })
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    }
+
+
+    // input on Change Logics
+
+    console.log(state);
+
+    const inputHandle = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+    // rotate logic
+
+    const leftRotate = () => {
+        setState({
+            ...state,
+            rotate: state.rotate - 90
+        })
+    }
+
+    const rightRotate = () => {
+        setState({
+            ...state,
+            rotate: state.rotate + 90
+        })
+    }
+
 
 
     return (
@@ -46,7 +113,7 @@ const ImageEditor = () => {
                                     <span>Filters</span>
                                     <div className='filter_key'>
                                         {
-                                            filterElement?.map((v, i) => <button key={i}>{v.name}</button>)
+                                            filterElement?.map((v, i) => <button className={property.name === v.name ? 'active' : ''} onClick={() => setProperty(v)} key={i}>{v.name}</button>)
                                         }
                                     </div>
                                 </div>
@@ -56,15 +123,15 @@ const ImageEditor = () => {
                                         <label>Rotate</label>
                                         <span>100%</span>
                                     </div>
-                                    <input type='range' min='0' max='200' />
+                                    <input name={property.name} onChange={inputHandle} value={state[property.name]} type='range' min='0' max={property.maxValue} />
                                 </div>
 
 
                                 <div className='rotate'>
                                     <label>Rotate & Flip</label>
                                     <div className='icon'>
-                                        <div><GrRotateLeft /></div>
-                                        <div><GrRotateRight /></div>
+                                        <div onClick={leftRotate}><GrRotateLeft /></div>
+                                        <div onClick={rightRotate}><GrRotateRight /></div>
                                         <div><CgMergeVertical /></div>
                                         <div><CgMergeHorizontal /></div>
                                     </div>
@@ -82,10 +149,13 @@ const ImageEditor = () => {
 
                         <div className='image_section'>
                             <div className='image'>
-                                <label htmlFor='choose'>
-                                    <IoIosImage />
-                                    <span>Choose Image</span>
-                                </label>
+                                {
+                                    state?.image ? <img style={{ filter: `brightness(${state?.brightness}%) grayscale(${state?.grayscale}%) sepia(${state?.sepia}%) saturate(${state?.saturate}%) contrast(${state?.contrast}%) hue-rotate(${state?.hueRotate}deg)`, transform: `rotate(${state?.rotate}deg) scale(${state?.vertical},${state?.horizontal})` }} src={state?.image} alt='image' /> : <label htmlFor='choose'>
+                                        <IoIosImage />
+                                        <span>Choose Image</span>
+                                    </label>
+                                }
+
                             </div>
 
                             <div className='image_select'>
@@ -93,7 +163,7 @@ const ImageEditor = () => {
                                 <button className='redo'><IoMdRedo /></button>
                                 <button className='crop'>Crop Image</button>
                                 <label htmlFor='choose'>Choose Image</label>
-                                <input type='file' id='choose' />
+                                <input onChange={imageHandle} type='file' id='choose' />
                             </div>
                         </div>
 
